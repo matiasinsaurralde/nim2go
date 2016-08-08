1c393c2683d1595b4464a48a397c52cf2bffe2b0
Matias Insaurralde <matias@insaurral.de>
2016-08-08 01:22:33 -0400

    First steps on AST construction

--- parser.go
+++ parser.go
@@ -6,6 +6,8 @@ import(
   "os"
   "strings"
   "os/exec"
+
+  "log"
 )
 
 const TempPrefix string = "go2nim"
@@ -19,7 +21,7 @@ dumpTree:
 // A basic data structure for the Nim parser.
 type Parser struct {
   AppendMacros bool
-  CompilerOutput []byte
+  CompilerOutput string
 }
 
 // Initializes a new parser.
@@ -57,12 +59,81 @@ func WrapWithMacros(filePath string) (tempFile *os.File, err error) {
   return tempFile, err
 }
 
-// Calls the Nim compiler and returns the stdout data.
-func NimCompile(filePath string) (output []byte, err error) {
-  output, err = exec.Command("nim", "compile", filePath).Output()
-  return output, err
+func whitespaceCount(s string) int {
+  var count int = 0
+  for _, ch := range s {
+    if ch != ' ' {
+      break
+    } else {
+      count++
+    }
+  }
+  return count
 }
 
+func findToken(s string, indentation int) Token {
+  tokenCharacters := []string{}
+
+  for i, ch := range s {
+    if i >= indentation {
+      if ch == ' ' {
+        break
+      }
+      tokenCharacters = append(tokenCharacters, string(ch))
+    }
+  }
+
+  tokenStr := strings.Join(tokenCharacters, "")
+  token := TokenLookup(tokenStr)
+
+  return Token(token)
+}
+
+// Calls the Nim compiler and returns the stdout data.
+func NimCompile(filePath string) (output string, err error) {
+  var outputBytes []byte
+  outputBytes, err = exec.Command("nim", "compile", filePath).Output()
+  return string(outputBytes), err
+}
+
+// Builds the AST.
+func(p *Parser) BuildAst() (err error) {
+  log.Print("Building AST")
+  log.Print( p.CompilerOutput )
+
+  var rootNode Node
+
+  rootNode = &StmtList{
+    List: make([]Statement, 0),
+  }
+
+
+  source := NimSource{}
+
+  lines := strings.Split(p.CompilerOutput, "\n")
+
+  var parentStructure *Node
+  var currentStructure *Node
+
+  for i, line := range lines {
+    if line != "" {
+      indentation := whitespaceCount(line)
+      token := findToken(line, indentation)
+      log.Println(i,"ws =", indentation, "token = ", token, line )
+
+      if token == Token(STMTLIST) {
+        source.Root = &rootNode
+        currentStructure = &rootNode
+        parentStructure = &rootNode
+      }
+
+      if token == Token(IMPORTSTMT) {
+        
+      }
+    }
+  }
+  return err
+}
 
 // Parses Nim code directly, eval-like. It makes use of a temporary file.
 func(p *Parser) ParseString(source string) (err error) {
@@ -77,6 +148,8 @@ func(p *Parser) ParseFile(filePath string) (err error) {
   if err == nil {
     p.CompilerOutput, err = NimCompile(sourceFile.Name())
     os.Remove(sourceFile.Name())
+
+    err = p.BuildAst()
   }
 
   return err
